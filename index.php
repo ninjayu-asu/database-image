ad5d87271dbb71f0963b9cf5f15d6bc99202660d
ninjayu-asu <168018970+ninjayu-asu@users.noreply.github.com>
2024-04-25 01:35:39 +0700

    Update index.php
    Ngapa Lo anjing kontol babi

--- index.php
+++ index.php
@@ -13,5 +13,10 @@
     "id": 1,
     "judul": "Kartun Wir",
     "url": "https://i.ibb.co/2dNWVv6/FB-IMG-1713981944555.jpg"
+  },
+  {
+    "id": 1,
+    "judul": "Kartun Wir",
+    "url": "https://i.ibb.co/BZ7Sx8Q/FB-IMG-1713983606863.jpg"
   }
 ]
