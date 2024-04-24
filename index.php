fd3fc53034e0e292f70cd0e30cfd6ee793a09170
ninjayu-asu <168018970+ninjayu-asu@users.noreply.github.com>
2024-04-25 01:08:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
     "url": "https://i.ibb.co/x76PGgb/FB-IMG-1713020437894.jpg"
   },
   {
-    "id": 2,
+    "id": 1,
     "judul": "Mantap Wir",
     "url": "https://i.ibb.co/wCt6fmL/Screenshot-484.png"
+  },
+  {
+    "id": 1,
+    "judul": "Mantap Wir",
+    "url": "https://i.ibb.co/2dNWVv6/FB-IMG-1713981944555.jpg"
   }
 ]
